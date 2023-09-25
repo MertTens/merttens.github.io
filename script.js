3531a886c30542034824cf8b667216e29b9950f3
Alex <mertensa@Alexs-MacBook-Pro.local>
2023-09-25 16:14:16 -0400

    Initial commit

--- script.js
+++ script.js
@@ -52,3 +52,108 @@ function togglePlayback() {
     }
 }
 
+
+// Get the canvas and context for doodling
+const doodleCanvas = document.getElementById('doodleCanvas');
+const doodleContext = doodleCanvas.getContext('2d');
+
+// Get the canvas and context for displaying an image
+const imageCanvas = document.getElementById('imageCanvas');
+const imageContext = imageCanvas.getContext('2d');
+
+// Function to set the canvas width based on the maximum available height
+function setCanvasWidth() {
+    // Get the maximum available height based on the container
+    const container = document.querySelector('.canvas-container');
+    const containerHeight = container.clientHeight; // Maximum available height
+
+    // Set the canvas width to a percentage of the maximum available height
+    const canvasSizePercentage = 1; // Adjust this as needed (e.g., 80% of maximum height)
+    const canvasWidth = containerHeight * canvasSizePercentage;
+
+    // Set the canvas width and height
+    doodleCanvas.width = canvasWidth;
+    imageCanvas.width = canvasWidth;
+    doodleCanvas.height = containerHeight;
+    imageCanvas.height = containerHeight;
+    drawImage();
+}
+
+// Call the setCanvasWidth function initially and when the container size changes
+window.addEventListener('resize', setCanvasWidth);
+setCanvasWidth(); // Call it initially to set canvas sizes
+
+// Set up drawing properties for doodling
+let drawing = false;
+
+// Event listeners for mouse actions on the doodle canvas
+doodleCanvas.addEventListener('mousedown', () => {
+    drawing = true;
+});
+
+doodleCanvas.addEventListener('mouseup', () => {
+    drawing = false;
+    doodleContext.beginPath();
+});
+
+doodleCanvas.addEventListener('mousemove', draw);
+
+// Function to draw on the doodle canvas
+function draw(e) {
+    if (!drawing) return;
+
+    const rect = doodleCanvas.getBoundingClientRect();
+    const scaleX = doodleCanvas.width / rect.width;
+    const scaleY = doodleCanvas.height / rect.height;
+
+    const x = (e.clientX - rect.left) * scaleX;
+    const y = (e.clientY - rect.top) * scaleY;
+
+    doodleContext.lineWidth = 5;
+    doodleContext.lineCap = 'round';
+    doodleContext.strokeStyle = 'white';
+
+    doodleContext.lineTo(x, y);
+    doodleContext.stroke();
+    doodleContext.beginPath();
+    doodleContext.moveTo(x, y);
+}
+
+// Clear button event listener for the doodle canvas
+const clearButton = document.getElementById('clearButton');
+clearButton.addEventListener('click', () => {
+    doodleContext.clearRect(0, 0, doodleCanvas.width, doodleCanvas.height);
+});
+
+// Rest of your JavaScript code...
+// Function to load and draw an image onto the canvas
+function drawImage() {
+    const img = new Image();
+    img.src = 'assets/brainmri.jpg'; // Replace with the path to your image
+
+    img.onload = () => {
+        imageCanvas.width = img.width;
+        imageCanvas.height = img.height;
+
+        // Draw the original image on the canvas
+        imageContext.drawImage(img, 0, 0);
+
+        // Get the image data from the canvas
+        const imageData = imageContext.getImageData(0, 0, imageCanvas.width, imageCanvas.height);
+        const data = imageData.data;
+        // Loop through the pixel data and change the color (e.g., from red to blue)
+        for (let i = 0; i < data.length; i += 4) {
+            // Modify the R, G, and B values (e.g., change red to blue)
+            data[i] = 0;       // Red channel
+            data[i + 1] = 0;   // Green channel
+            data[i + 2] = 0; // Blue channel
+            // Alpha channel (data[i + 3]) remains unchanged
+        }
+
+        // Put the modified image data back on the canvas
+        context.putImageData(imageData, 0, 0);
+    };
+}
+
+// Call the drawImage function to load and draw the image
+drawImage();
